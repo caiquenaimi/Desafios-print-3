2b81c3d556a6ee5ccb79ccb5ae23c447c6af4c24
Caique <caiquenaimi.564@gmail.com>
2023-05-22 17:11:21 -0300

    first commit

--- assets/javascript/painel-administrativo.js
+++ assets/javascript/painel-administrativo.js
@@ -3,18 +3,17 @@ function add() {
     var nome = document.getElementById("input1").value;
     var resumo = document.getElementById("input2").value;
     var publi = document.getElementById("input3").value;
-
     var autor = document.getElementById("input5").value;
     var fixpubli = publi.split("-").reverse().join("/");
     var nome1 = [];
     var resumo1 = [];
     var publi1 = [];
- 
     var autor1 = [];
-
     var arr = [];
-  
+    
 
+
+    
     var modal = document.getElementById("modal-container");
     nome1.push(nome);
     resumo1.push(resumo);
@@ -24,6 +23,7 @@ function add() {
     console.log(arr);
     modal.innerHTML = `<section><h1 id="h1">${nome1}</h1><br><p id="p">${resumo1}</p><br><p id="p">${publi1}</p><br><p id="p">${autor1}</p></section>`;
     
+    
     var nome = document.getElementById("input1").value = '';
     var resumo = document.getElementById("input2").value = '';
     var publi = document.getElementById("input3").value = '';
@@ -31,6 +31,6 @@ function add() {
     var autor = document.getElementById("input5").value = '';
 
     modal.innerHTML = `<section><h1 id="h1">${nome1}</h1><br><p id="p">${resumo1}</p><br><p id="p">${publi1}</p><br><p id="p">${autor1}</p></section>`;
-    
+
 }
 
