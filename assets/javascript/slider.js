c8aa6290ebf4fe96231016ced3c522f3a6c9ac3a
pravel <123407140+ElMatheus@users.noreply.github.com>
2023-06-05 10:55:08 -0300

    finish index

--- assets/javascript/slider.js
+++ assets/javascript/slider.js
@@ -12,22 +12,27 @@ function recarregar() {
 
 
 function avancar() {
-        if (contagem == 6){
-            contagem = 0;
-        }
-        contagem++;
-        document.getElementById('img1').style.display = 'none';
-        document.getElementById('img2').style.display = 'none';
-        document.getElementById('img3').style.display = 'none';
-        document.getElementById('img4').style.display = 'none';
-        document.getElementById('img5').style.display = 'none';
-        document.getElementById('img6').style.display = 'none';
-        document.getElementById('img' + contagem).style.display = 'block';
-    
-    
+
+    if (contagem == 6) {
+        contagem = 0;
+    }
+    contagem++;
+    document.getElementById('img1').style.display = 'none';
+    document.getElementById('img2').style.display = 'none';
+    document.getElementById('img3').style.display = 'none';
+    document.getElementById('img4').style.display = 'none';
+    document.getElementById('img5').style.display = 'none';
+    document.getElementById('img6').style.display = 'none';
+    document.getElementById('img' + contagem).style.display = 'block';
+
+
 }
 
 function desavancar() {
+
+    if (contagem == 1){
+        contagem = 7;
+    }
     contagem--;
     document.getElementById('img1').style.display = 'none';
     document.getElementById('img2').style.display = 'none';
