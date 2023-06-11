a950df940ca8a8937e03da2ad4934424bdf58f50
Caique <caiquenaimi.564@gmail.com>
2023-06-11 20:24:21 -0300

    fix painel dnv

--- assets/javascript/painel-administrativo.js
+++ assets/javascript/painel-administrativo.js
@@ -4,65 +4,66 @@ let data = [];
 let cate = [];
 let autor = [];
 let contador;
-let editTitle = '';
-
-
-
-
-let i = 0;
 
 function add() {
-    let inputTitle = document.getElementById('input1').value;
-    let inputResum = document.getElementById('input2').value;
-    let inputData = document.getElementById('input3').value.split('-').reverse().join('-');
-    let inputCate = document.getElementById('input4').value;
-    let inputAutor = document.getElementById('input5').value;
+  let inputTitle = document.getElementById('input1').value;
+  let inputResum = document.getElementById('input2').value;
+  let inputData = document.getElementById('input3').value.split('-').reverse().join('-');
+  let inputCate = document.getElementById('input4').value;
+  let inputAutor = document.getElementById('input5').value;
 
-    if (inputTitle === '' || inputResum === '' || inputData === '' || inputCate === '' || inputAutor === '') {
-        let msg = document.getElementById('p1');
-        msg.innerHTML = 'Preencha o formulário antes de adicionar um artigo';
-        return;
-    }
-
+  if (inputTitle === '' || inputResum === '' || inputData === '' || inputCate === '' || inputAutor === '') {
     let msg = document.getElementById('p1');
-    msg.innerHTML = '';
+    msg.innerHTML = 'Preencha o formulário antes de adicionar um artigo';
+    return;
+  }
 
+  let msg = document.getElementById('p1');
+  msg.innerHTML = '';
+
+  if (contador !== undefined) {
+    // Atualizar os valores do item existente
+    title[contador] = inputTitle;
+    resum[contador] = inputResum;
+    data[contador] = inputData;
+    cate[contador] = inputCate;
+    autor[contador] = inputAutor;
+    contador = undefined; // Redefinir o contador
+  } else {
+    // Adicionar um novo item
     title.push(inputTitle);
     resum.push(inputResum);
     data.push(inputData);
     cate.push(inputCate);
     autor.push(inputAutor);
+  }
 
-    let i = 0;
-    let conteudo = '';
+  let conteudo = '';
 
-    while (i < title.length) {
-        conteudo += '<section id="result' + i + '">';
-        conteudo += '<h2>' + title[i] + '</h2>';
-        conteudo += '<p class="resumos">' + resum[i] + '</p>';
-        conteudo += '<p class="autores">' + 'por: ' + '<strong>' + autor[i] + '</strong>' + '</p>';
-        conteudo += '<p class="categorias">' + 'de: ' + cate[i] + '</p>';
-        conteudo += '<p class="datas" id="date">' + data[i] + '</p>';
-        conteudo += '<button id="btn1" onclick="edt(' + "'" + i + "'" + ')">Editar🖊</button>';
-        conteudo += '<button id="btn2" onclick="rmv(' + "'" + i + "'" + ')">Remover🗑</button>';
-        conteudo += '</section>';
+  for (let i = 0; i < title.length; i++) {
+    conteudo += '<section id="result' + i + '">';
+    conteudo += '<h2>' + title[i] + '</h2>';
+    conteudo += '<p class="resumos">' + resum[i] + '</p>';
+    conteudo += '<p class="autores">' + 'por: ' + '<strong>' + autor[i] + '</strong>' + '</p>';
+    conteudo += '<p class="categorias">' + 'de: ' + cate[i] + '</p>';
+    conteudo += '<p class="datas" id="date">' + data[i] + '</p>';
+    conteudo += '<button id="btn1" onclick="edt(' + i + ')">Editar🖊</button>';
+    conteudo += '<button id="btn2" onclick="rmv(' + i + ')">Remover🗑</button>';
+    conteudo += '</section>';
+  }
 
-        i++;
-    }
-
-    document.getElementsByTagName('main')[0].innerHTML = conteudo;
+  document.getElementsByTagName('main')[0].innerHTML = conteudo;
 }
 
 function rmv(hidden) {
-    document.getElementById('result' + hidden).style.display = 'none';
+  document.getElementById('result' + hidden).style.display = 'none';
 }
 
 function edt(edit) {
-    document.getElementById('input1').value = title[edit];
-    document.getElementById('input2').value = resum[edit];
-    document.getElementById('input3').value = data[edit];
-    document.getElementById('input4').value = cate[edit];
-    document.getElementById('input5').value = autor[edit];
-    contador = edit;
-
+  document.getElementById('input1').value = title[edit];
+  document.getElementById('input2').value = resum[edit];
+  document.getElementById('input3').value = data[edit];
+  document.getElementById('input4').value = cate[edit];
+  document.getElementById('input5').value = autor[edit];
+  contador = edit;
 }
